ef799c2f5ecad6a34f358b659d775e645798ccb5
Samson Mathews <samsonmmathews@gmail.com>
2025-09-11 16:40:49 -0400

    Added pug file

---
+++ Week 2/Test/index.js
@@ -0,0 +1,28 @@
+// IMPORT REQUIRED MODULES
+import express, { response } from "express";
+import { request } from "http";
+import path from "path"; // to make available methods for path concatenation, etc.
+
+const __dirname = import.meta.dirname;
+
+const app = express(); //create Express app
+const port = process.env.PORT || "8888";
+
+//set up templates in Express app
+app.set("views", path.join(__dirname, "templates")); // for Mac users, it's recommended to use "views" as the template folder name (most reliable)
+app.set("view engine", "pug"); //set app to use "Pug" as the template engine
+
+// HOME PAGE
+app.get("/", (request, response) => {
+    response.render("index")
+});
+
+// test app to see if it's working
+// app.get("/", (request, response) => {
+//     response.status(200).send("Random text to display");
+// });
+
+//set server to listen for incoming requests
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
